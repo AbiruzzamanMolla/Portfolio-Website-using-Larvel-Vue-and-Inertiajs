e7223c79abe1ed16d84a6b057c5ff4ec67d0f678
Abir <abiruzzaman.molla@gmail.com>
2022-10-21 13:26:05 +0600

    feat: show projectrs

---
+++ resources/js/Components/Frontend/Portfolio.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Projects from "./Projects.vue";
+defineProps({
+  skills: Object,
+  projects: Object,
+});
+</script>
+<template>
+  <section
+    id="portfolio"
+    class="section bg-light-primary dark:bg-dark-primary min-h-[1400px]"
+  >
+    <div class="container mx-auto">
+      <div class="flex flex-col items-center text-center">
+        <h2 class="section-title">My latest works</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque veniam debitis
+          fuga officiis tempore temporibus odio nulla quos, distinctio eligendi
+          perferendis accusantium facere dolore praesentium. Tenetur totam dicta repellat
+          incidunt.
+        </p>
+      </div>
+      <Projects :skills="skills" :projects="projects" />
+    </div>
+  </section>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,16 +1,32 @@
 <script setup>
 import { Head, Link } from "@inertiajs/inertia-vue3";
 import FrontendLayout from "@/Layouts/Frontend.vue";
+import Hero from "@/Components/Frontend/Hero.vue";
+import About from "../Components/Frontend/About.vue";
+import Promotion from "../Components/Frontend/Promotion.vue";
+import Skills from "../Components/Frontend/Skills.vue";
+import Portfolio from "@/Components/Frontend/Portfolio.vue";
+
+defineProps({
+  skills: Object,
+  projects: Object,
+});
 </script>
 
 <template>
   <Head title="Welcome to Portfolio" />
   <FrontendLayout>
     <!-- hero section - primary -->
+    <Hero />
     <!-- promote section - tail -100 -->
+    <Promotion />
     <!-- about section secoundary color -->
+    <About />
     <!-- skills sections tail - 100 -->
+    <Skills :skills="skills" />
     <!-- portfolio primary -->
+    <Portfolio :skills="skills" :projects="projects" />
     <!-- services secoundary -->
     <!-- contact section primary -->
+  </FrontendLayout>
 </template>
